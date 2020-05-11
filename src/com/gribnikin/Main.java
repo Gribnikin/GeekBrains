df6e4816f3e8445f177abddec3c2f3bb1086712c
Gribnikin <anikoanton@gmail.com>
2020-05-11 21:48:07 +0300

    1234

--- src/com/gribnikin/Main.java
+++ src/com/gribnikin/Main.java
@@ -3,8 +3,8 @@ package com.gribnikin;
 public class Main {
 
     public static void main(String[] args) {
-        String a = "Hello %s";
-        System.out.println(String.format(a,"World !"));
+        String a = "Hello hello %s";
+        System.out.println(String.format(a,"World!"));
 	// write your code here
     }
 }
