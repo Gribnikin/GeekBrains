0188e74070a48ee6c7bd8ce4c01127cf99ebaf56
Gribnikin <anikoanton@gmail.com>
2020-05-15 23:28:13 +0300

    1234

--- src/com/gribnikin/Main.java
+++ src/com/gribnikin/Main.java
@@ -1,10 +1,54 @@
 package com.gribnikin;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    /*
+
+    TASK 3.
+    1. Написать программу, которая загадывает случайное число от 0 до 9,
+    и пользователю дается 3 попытки угадать это число.
+    При каждой попытке компьютер должен сообщить больше ли указанное пользователем число чем загаданное, или меньше.
+    После победы или проигрыша выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
+    */
 
     public static void main(String[] args) {
-        String a = "Hello hello %s";
-        System.out.println(String.format(a,"World!"));
-	// write your code here
+       task3_zagadat_chislo();
+
     }
-}
+
+    static void task3_zagadat_chislo()
+    {
+        int tryCount=0;
+        int chislo = (int)(Math.random()*10);
+        boolean win=false;
+        boolean exit=false;
+        int tryChislo;
+        Scanner in = new Scanner(System.in);
+        while (!exit)
+        {
+
+            System.out.println ("Загадайте число от 0 до 9 за 3 поптыки" );
+            tryChislo = in.nextInt();
+            if (tryChislo==chislo)
+            win=true;
+            else
+            {
+                if (tryChislo<chislo)
+                System.out.println("Ваше число меньше определенного!");
+                else
+                System.out.println("Ваше число больше определенного!");
+            };
+
+            tryCount++;
+            if ((tryCount==3)||win)
+                exit=true;
+        }
+        if (win)
+            System.out.println("Вы выйграли");
+        else
+            System.out.format("Вы проиграли, загаданное число - %s", chislo);
+     }
+    }
+
